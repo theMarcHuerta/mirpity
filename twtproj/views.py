c38c5eef998e6457e966ecaceff4463b74bf4ee2
Marc <63676857+theMarcHuerta@users.noreply.github.com>
2021-12-17 04:58:26 -0800

    test

--- twtproj/views.py
+++ twtproj/views.py
@@ -15,7 +15,7 @@ def output(request):
 def external(request):
     inp = request.POST.get('param')
 #     out = run([sys.executable,'//Users//ironmacbookair//Desktop//twtproj//twtproj//twt_code.py', inp], shell=False, stdout=PIPE)
-    out = run([sys.executable,'//theMarcHuerta//mirpity//twtproj//twt_code.py', inp], shell=False, stdout=PIPE)
+    out = run([sys.executable,'//twt_code.py', inp], shell=False, stdout=PIPE)
     rout = str(out.stdout) + "HELLO"
     return render(request, 'home.html', {'data1':rout})
 
